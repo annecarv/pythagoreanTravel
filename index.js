616360265b053dc07af3d299e26e1f1567b94173
annetherezaprofissional@gmai.com <annetherezaprofissional@gmail.com>
2022-10-04 11:20:42 -0300

    fix: formule

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-
-var form = document.getElementById('dadosFormulario')
 var valueHipotenusa = document.getElementById('hipotenusaInput')
+var form = document.getElementById('dadosFormulario')
 var valueCatetoA = document.getElementById('catetoAInput')
 var valueCatetoB = document.getElementById('catetoBInput')
 var button = document.getElementById('calculateButton')
 
 form.addEventListener('submit',  function calcularHipotenusa(event) {
     event.preventDefault()
-    let data = {
-        valueHipotenusa: parseInt(valueHipotenusa.value),
-        valueCatetoA: parseInt(valueCatetoA.value),
-        valueCatetoB : parseInt(valueCatetoB.value)
-    }
-    data.valueHipotenusa = Math.pow(data.valueHipotenusa, 2)
-    data.valueCatetoA = Math.pow(data.valueCatetoA, 2)
-    data.valueCatetoB = Math.pow(data.valueCatetoB, 2)
-    let resultadoTheorema = data.valueHipotenusa + data.valueCatetoA + data.valueCatetoB
+
+    let catetoA = valueCatetoA.value
+    let catetoB = valueCatetoB.value
+
+    let resultadoTheorema
+    resultadoTheorema = Math.hypot(catetoA, catetoB).toFixed(2)
+  
+    let total = document.getElementById('resultadoTeoremaDePitagoras').innerHTML = `${resultadoTheorema.toString()}`
+    console.log(total)
+    console.log(catetoA)
+    console.log(catetoB)
 
 
-    let teste = document.getElementById('resultadoTeoremaDePitagoras').innerHTML = `${resultadoTheorema.toString()}`
-    return teste
 })
